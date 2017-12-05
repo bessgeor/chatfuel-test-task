62490877ea91d903c9d51071524e4c9303944459
bessgeor <bessgeor@gmail.com>
2017-12-06 02:46:15 +0300

    Added input possibility

---
+++ ChatfuelTestTask/InputController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ChatfuelTestTask
+{
+	[Route( "" )]
+	public class InputController : Controller
+    {
+		private readonly Lift _lift;
+
+		public InputController( Lift lift ) => _lift = lift;
+
+		[HttpGet( "call-from/{floor}" )]
+		public Task CallFrom( byte floor )
+			=> _lift.CallFromOutside( floor );
+
+		[HttpGet( "direct-to/{floor}" )]
+		public Task DirectTo( byte floor )
+			=> _lift.DirectFromInside( floor );
+    }
+}
