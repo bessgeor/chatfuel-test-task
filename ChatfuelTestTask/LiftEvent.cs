6e3e81a5d25fc67174208511f3557ae803ded761
bessgeor <bessgeor@gmail.com>
2017-12-06 02:45:13 +0300

    Added lift event struct

---
+++ ChatfuelTestTask/LiftEvent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ChatfuelTestTask
+{
+	public enum LiftEventType
+	{
+		ArrivedToFloor,
+		DoorsOpened,
+		DoorsClosed
+	}
+
+    public struct LiftEvent : IEquatable<LiftEvent>
+	{
+		public DateTime OccuredAt { get; }
+		public LiftEventType Type { get; }
+		public byte Floor { get; }
+
+		public LiftEvent( LiftEventType type, byte floor )
+		{
+			OccuredAt = DateTime.UtcNow;
+			Floor = floor;
+			Type = type;
+		}
+
+		/// <summary>
+		/// Outputs itself to a console. Should only be used from outside of ThreadPool because of thread locking
+		/// </summary>
+		public void WriteToConsoleLocking() // ToString() are called to avoid boxing
+			=> Console.WriteLine( $"{OccuredAt.Hour.ToString()}:{OccuredAt.Minute.ToString()}:{OccuredAt.Second.ToString()}.{OccuredAt.Millisecond.ToString()} UTC: on floor {Floor.ToString()} {Type.ToString()}" );
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				const int prime = -1521134295;
+				int hash = 12345701;
+				hash = hash * prime + System.Collections.Generic.EqualityComparer<DateTime>.Default.GetHashCode( OccuredAt );
+				hash = hash * prime + System.Collections.Generic.EqualityComparer<LiftEventType>.Default.GetHashCode( Type );
+				hash = hash * prime + System.Collections.Generic.EqualityComparer<byte>.Default.GetHashCode( Floor );
+				return hash;
+			}
+		}
+
+		public bool Equals( LiftEvent other ) => OccuredAt == other.OccuredAt && System.Collections.Generic.EqualityComparer<LiftEventType>.Default.Equals( Type, other.Type ) && System.Collections.Generic.EqualityComparer<byte>.Default.Equals( Floor, other.Floor );
+		public override bool Equals( object obj ) => obj is LiftEvent && Equals( (LiftEvent) obj );
+
+		public static bool operator ==( LiftEvent x, LiftEvent y ) => x.Equals( y );
+		public static bool operator !=( LiftEvent x, LiftEvent y ) => !x.Equals( y );
+	}
+}
