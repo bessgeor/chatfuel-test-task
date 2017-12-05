d14a4dd6c4879a931dabe366c163ac83c3ab6cfa
bessgeor <bessgeor@gmail.com>
2017-12-06 02:56:47 +0300

    Added output provider

---
+++ ChatfuelTestTask/OutputProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatfuelTestTask
+{
+	public class OutputProvider
+	{
+		public void Add( LiftEvent item )
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<LiftEvent> TakeAsync( CancellationToken cancellationToken )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
