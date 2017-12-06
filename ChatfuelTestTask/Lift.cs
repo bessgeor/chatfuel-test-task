a831b37796f6522d08806a12c625d5f91960ba29
bessgeor <bessgeor@gmail.com>
2017-12-06 23:43:34 +0300

    Initted basic lift state

--- ChatfuelTestTask/Lift.cs
+++ ChatfuelTestTask/Lift.cs
@@ -5,9 +5,16 @@ namespace ChatfuelTestTask
 {
 	public class Lift
     {
+		private readonly OutputProvider _out = new OutputProvider();
+		private readonly TimeSpan _doorsFreeze;
+		private readonly TimeSpan _floorFreeze;
+		private readonly byte _numberOfStoreys;
+
 		internal Lift( Config config )
 		{
-
+			_doorsFreeze = TimeSpan.FromSeconds( config.LiftDoorsOpenCloseTime );
+			_floorFreeze = TimeSpan.FromSeconds( ( (double) config.StoreyHeight ) / config.LiftSpeed );
+			_numberOfStoreys = config.NumberOfStoreys;
 		}
 
 		public Task CallFromOutside( byte floor )
