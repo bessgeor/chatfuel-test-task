f986e330e067931ca5a4c2cdf9b4381837df8f0c
bessgeor <bessgeor@gmail.com>
2017-12-06 23:45:34 +0300

    Added lift class stub

---
+++ ChatfuelTestTask/Lift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChatfuelTestTask
+{
+	public class Lift
+    {
+		internal Lift( Config config )
+		{
+
+		}
+
+		public Task CallFromOutside( byte floor )
+			=> Task.FromException( new NotImplementedException() );
+
+		public Task DirectFromInside( byte floor )
+			=> Task.FromException( new NotImplementedException() );
+	}
+}
